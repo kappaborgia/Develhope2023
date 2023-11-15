3a9a514bc392a065fbae18c231876a8ce0c96ebe
kappa-git <kevin.borgia@outlook.it>
2023-11-16 00:05:32 +0100

    Ereditarietà/calcola area

---
+++ src/JavaBasic/modulo3/ex2/Forma.java
@@ -0,0 +1,14 @@
+package JavaBasic.modulo3.ex2;
+/* Scrivere un programma che contenga una classe chiamata Forma ed un metodo chiamato calcolaArea() 
+che stampi l'area della forma.
+Crea poi una sottoclasse chiamata Rettangolo che fa override del metodo calcolaArea() 
+per calcolare l'area del rettangolo. */
+public class Forma {
+
+    public double calcolaArea(){
+        System.out.println("Calcolo delle seguenti aree: ");
+        return 0;
+    }
+
+
+}

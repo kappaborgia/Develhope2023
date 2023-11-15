3f8c3f6f02140e992535827d5dac33a90fc52e7a
kappa-git <kevin.borgia@outlook.it>
2023-11-16 00:04:43 +0100

    ereditarietà/verso animale

---
+++ src/JavaBasic/modulo3/ex1/Gatto.java
@@ -0,0 +1,11 @@
+package JavaBasic.modulo3.ex1;
+
+public class Gatto extends Animale{
+
+   @Override
+   public void faiIlVerso(){
+        System.out.println("Io sono un gatto: " + "mewwww");
+
+   }    
+    
+}
---
+++ src/ex1/Animale.java
@@ -0,0 +1,9 @@
+package module3.ex1;
+
+public class Animale {
+    // crea un metodo per fare il verso dell'animale
+    public void faiIlVerso(){
+        System.out.println("Io sono un cane: " + "Wof Wof");
+
+    }
+}

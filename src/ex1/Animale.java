10b99306610265c5b8b3682f7aad4dfb3e71fbdd
kappa-git <kevin.borgia@outlook.it>
2023-11-16 19:50:26 +0100

    delete

--- src/ex1/Animale.java
+++
@@ -1,9 +0,0 @@
-package module3.ex1;
-
-public class Animale {
-    // crea un metodo per fare il verso dell'animale
-    public void faiIlVerso(){
-        System.out.println("Io sono un cane: " + "Wof Wof");
-
-    }
-}

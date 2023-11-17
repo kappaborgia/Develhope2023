a1b6c867000a4ed90afaf61fb3376c810a74cebf
kappa-git <kevin.borgia@outlook.it>
2023-11-17 20:51:46 +0100

    get/setter

---
+++ src/JavaBasic/modulo2/ex3/Auto.java
@@ -0,0 +1,57 @@
+package JavaBasic.modulo2.ex3;
+
+public class Auto {
+    private String Brand;
+    private String Model;
+    private int cc;
+    private int cv;
+    private String Targa;
+
+    public Auto(String marca, String modello, int cc, int cv, String targa){
+        this.Brand = marca;
+        this.Model = modello;
+        this.cc = cc;
+        this.cv = cv;
+        this.Targa = targa;
+    }
+
+    ///////////////////////////////////////
+    public String getbrand(){
+        return Brand;
+    }
+    public void setbrand(String Marca){
+        this.Brand = Marca;
+    }
+
+    ///////////////////////////////////////
+    public String getmodel(){
+        return Model;
+    }
+    public void setmodel(String modello){
+        this.Model = modello;
+    }
+
+    //////////////////////////////////////
+    public int getcc(){
+        return cc;
+    }
+    public void setcc(int cc){
+        this.cc = cc;
+    }
+
+    ////////////////////////////////////
+    public int getcv(){
+        return cv;
+    }
+    public void setcv(int cv){
+        this.cv = cv;
+    }
+
+    ////////////////////////////////////////
+    public String gettarga(){
+        return Targa;
+    }
+    public void settarga(String targa){
+        this.Targa = targa;
+    }
+}
---
+++ src/JavaBasic/modulo2/ex3/Main.java
@@ -0,0 +1,59 @@
+package JavaBasic.modulo2.ex3;
+
+/* Scrivere un programma che contenga una classe che definisce un Auto che abbia come proprietà cilindrata,
+ * targa, marca e modello.
+ */
+
+import JavaBasic.modulo2.ex2.Auto;
+
+public class Main {
+    public static void main(String[] args) {
+        Auto veicolo1 = new Auto("TOYOTA", "Supra", 2998, 340, "ET435FG");
+        System.out.println("MARCA: " + veicolo1.getbrand());
+        System.out.println("MODELLO: " + veicolo1.getmodel());
+        System.out.println(veicolo1.getcc() + " CC");
+        System.out.println(veicolo1.getcv() + " CV");
+        System.out.println("TARGA: " + veicolo1.gettarga());
+        System.out.println();
+
+        veicolo1.setbrand("BMW");
+        veicolo1.setmodel("M4");
+        veicolo1.setcc(2998);
+        veicolo1.setcv(431);
+        veicolo1.settarga("GF178DB");
+        System.out.println("MARCA: " + veicolo1.getbrand());
+        System.out.println("MODELLO: " + veicolo1.getmodel());
+        System.out.println(veicolo1.getcc() + " CC");
+        System.out.println(veicolo1.getcv() + " CV");
+        System.out.println("TARGA: " + veicolo1.gettarga());
+        System.out.println();
+
+        veicolo1.setbrand("VOLVO");
+        veicolo1.setmodel("XC60");
+        veicolo1.setcc(1969);
+        veicolo1.setcv(455);
+        veicolo1.settarga("FE749NP");
+        System.out.println("MARCA: " + veicolo1.getbrand());
+        System.out.println("MODELLO: " + veicolo1.getmodel());
+        System.out.println(veicolo1.getcc() + " CC");
+        System.out.println(veicolo1.getcv() + " CV");
+        System.out.println("TARGA: " + veicolo1.gettarga());
+        System.out.println();
+
+        veicolo1.setbrand("FIAT");
+        veicolo1.setmodel("CINQUECENTO");
+        veicolo1.setcc(899);
+        veicolo1.setcv(39);
+        veicolo1.settarga("AL438CF");
+        System.out.println("MARCA :" + veicolo1.getbrand());
+        System.out.println("MODELLO :" + veicolo1.getmodel());
+        System.out.println(veicolo1.getcc() + " CC");
+        System.out.println(veicolo1.getcv() + " CV");
+        System.out.println("TARGA :" + veicolo1.gettarga());
+
+
+        
+    }
+
+
+}

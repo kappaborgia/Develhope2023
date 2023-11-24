4eb9416a9944f76879589898e5b3672e5c776d9b
kappa-git <kevin.borgia@outlook.it>
2023-11-24 18:38:40 +0100

    Exception/try catch

--- src/JavaAdvanced/modulo1/ex3/Main.java
+++ src/JavaAdvanced/modulo1/ex3/Main.java
@@ -1,2 +1,18 @@
-package JavaAdvanced.modulo1.ex3;public class Main {
+package JavaAdvanced.modulo1.ex3;
+/* Scrivere una funzione che provi a dividere un numero per zero e catturi l'eccezione leggendone il
+* messaggio*/
+public class Main {
+    public static void main(String[] args){
+
+        try{
+
+            int num = 4;
+            int result = num / 0;
+            System.out.println("Risultato: " + result);
+
+        } catch(ArithmeticException e){
+            System.out.println("Non puoi dividere per zero");
+        }
+    }
+
 }

9a76f3ad706809143fc10aa868826b607f230c38
kappa-git <kevin.borgia@outlook.it>
2023-11-28 17:33:55 +0100

    HashSet

--- src/JavaAdvanced/modulo1/ex8/Main.java
+++ src/JavaAdvanced/modulo1/ex8/Main.java
@@ -1,2 +1,23 @@
-package JavaAdvanced.modulo1.ex8;public class Main {
+package JavaAdvanced.modulo1.ex8;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+/* Scrivere una funzione che restituisca un HashSet riempito
+Creare un oggetto dello stesso tipo inserito nell'HashSet e popolarlo
+Verificare che l' elemento sia parte del Set e stampare il risultato*/
+public class Main {
+    public static void main(String[] args) {
+        HashSet();
+
+    }
+    public static void HashSet (){
+        HashSet<String> notebook = new HashSet<String>(Arrays.asList("Zenbook14","Macbook air","xps"));
+        HashSet<String> notebook2 = new HashSet<>();
+        notebook2 = notebook;
+        notebook2.add("Pavillon");
+        System.out.println(notebook);
+
+    }
+    /* Non so se ho capito bene la traccia dell'esercizio, ma ho provato ad interpretarla */
 }

919895d48c4f20c9695e4de0bb87ed52fd7cb636
kappa-git <kevin.borgia@outlook.it>
2023-11-28 17:03:53 +0100

    HashSet

--- src/JavaAdvanced/modulo1/ex7/Main.java
+++ src/JavaAdvanced/modulo1/ex7/Main.java
@@ -1,2 +1,19 @@
-package JavaAdvanced.modulo1.ex7;public class Main {
+package JavaAdvanced.modulo1.ex7;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/* Scrivere una funzione che restituisca un hashset riempito
+Leggere l'hashSet e stampre grandezza ed elementi
+Il tuo esercizio */
+public class Main {
+    public static void main(String[] args) {
+        functionHashSet();
+    }
+    public static void functionHashSet(){
+        Set<Integer> numbers = new HashSet<Integer>(Arrays.asList(1,2,3,4));
+        System.out.println("Elementi dell'HashSet: " + numbers);
+        System.out.println("Grandezza dell'HashSet: " + numbers.size());
+    }
 }

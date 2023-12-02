98c4a7539da53c3a95a99622588a913bc6ed93d4
kappa-git <kevin.borgia@outlook.it>
2023-12-02 13:09:39 +0100

    Ordinare un ArrayList

--- src/JavaAdvanced/modulo2/ex1/Studente.java
+++ src/JavaAdvanced/modulo2/ex1/Studente.java
@@ -1,2 +1,18 @@
-package JavaAdvanced.modulo2.ex1;public class Studente {
+package JavaAdvanced.modulo2.ex1;
+
+public class Studente {
+    String name;
+    int age;
+
+    public Studente(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return '\n' + "Studente:" + '\n' +
+                "name = " + name + '\n' +
+                "age = " + age + '\n' + "-----------";
+    }
 }

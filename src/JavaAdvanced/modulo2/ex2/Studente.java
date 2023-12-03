0802016875a87e45430b687c950bd331b12b448e
kappa-git <kevin.borgia@outlook.it>
2023-12-03 01:00:37 +0100

    Arraylist

--- src/JavaAdvanced/modulo2/ex2/Studente.java
+++ src/JavaAdvanced/modulo2/ex2/Studente.java
@@ -1,2 +1,27 @@
-package JavaAdvanced.modulo2.ex2;public class Studente {
+package JavaAdvanced.modulo2.ex2;
+/*
+Creare una classe Student che accenti nel costruttore il parametro name (String e age Int)
+In questa classe avere i metodi pubblici per ottenere le informazioni.
+Creare un ArrayList con 12 elementi e stamparlo in console.
+mettere in ordine la collezione e stampare il risultato*/
+public class Studente {
+    String name;
+    int age;
+
+    public Studente(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    @Override
+    public String toString() {
+        return  '\n' + "Studente:" + '\n' +
+                "name = " + name + '\n' +
+                "age = " + age + '\n' + "--------------";
+    }
 }

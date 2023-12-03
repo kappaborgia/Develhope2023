680e8acd893d417bfe00c02bd49925df1931f490
kappa-git <kevin.borgia@outlook.it>
2023-12-03 20:36:15 +0100

    LinkedList

--- src/JavaAdvanced/modulo2/ex3/Fruit.java
+++ src/JavaAdvanced/modulo2/ex3/Fruit.java
@@ -1,2 +1,25 @@
-package JavaAdvanced.modulo2.ex3;public class Fruit {
+package JavaAdvanced.modulo2.ex3;
+/*Creare una classe Fruit che accenti nel costruttore il parametro name (String)
+In questa classe avere i metodi pubblici per ottenere le informazioni.
+Creare un LinkedList con elementi e stamparlo in console.
+Aggiungere un elemento al primo posto della lista e uno all'ultimo
+Stampare la collezione aggiornata*/
+public class Fruit {
+    private String name;
+
+    public Fruit(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return '\n' + "Fruit" + '\n' +
+                "name: " + name + '\n';
+    }
+
+
 }

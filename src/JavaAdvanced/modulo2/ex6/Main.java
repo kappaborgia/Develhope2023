268ec3ed29c4676f4b50913caf4ce63bce1c8f97
kappa-git <kevin.borgia@outlook.it>
2023-12-03 20:37:30 +0100

    first commit

--- src/JavaAdvanced/modulo2/ex6/Main.java
+++ src/JavaAdvanced/modulo2/ex6/Main.java
@@ -1,2 +1,4 @@
-package JavaAdvanced.modulo2.ex6;public class Main {
+package JavaAdvanced.modulo2.ex6;
+/*Definisci un oggetto Record che rappresenti un Punto con le coordinate x & y*/
+public class Main {
 }

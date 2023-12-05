bf557ef5860cfee2e0ff1392f8ceaa7baf84e366
kappa-git <kevin.borgia@outlook.it>
2023-12-05 15:52:44 +0100

    HashMap

--- src/JavaAdvanced/modulo2/ex5/Main.java
+++ src/JavaAdvanced/modulo2/ex5/Main.java
@@ -1,4 +1,21 @@
 package JavaAdvanced.modulo2.ex5;
+
+import java.util.*;
+
 /*Creare un hashmap prendere tutti i valori, ordinarli e stamparli*/
 public class Main {
+    public static void main(String[] args) {
+        CreateAndPrint();
+    }
+    public static void CreateAndPrint(){
+        Map<Integer, String> Q1 = new LinkedHashMap<>(Map.ofEntries(
+                new AbstractMap.SimpleEntry<>(1,"Max Verstappen"),
+                new AbstractMap.SimpleEntry<>(2,"Lewis Hamilton"),
+                new AbstractMap.SimpleEntry<>(3,"Charles Leclerc")
+
+        ));
+        System.out.println("Qualifica: " + Q1);
+
+    }
 }
+
--- src/JavaAdvanced/modulo2/ex5/Prove.java
+++ src/JavaAdvanced/modulo2/ex5/Prove.java
@@ -1,2 +1,17 @@
-package JavaAdvanced.modulo2.ex5;public class Prove {
+package JavaAdvanced.modulo2.ex5;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Prove {
+    public static void main(String[] args) {
+        OrderAndPrint();
+    }
+    public static void OrderAndPrint(){
+        Map<Integer,String> Q2 = new LinkedHashMap<>();
+        Q2.put(1,"max");
+        Q2.put(2,"ff");
+        Q2.put(3,"dd");
+        System.out.println(Q2);
+    }
 }

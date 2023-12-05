11389e9027ec03f21e29ed7559326340974a68e3
kappa-git <kevin.borgia@outlook.it>
2023-12-05 16:54:23 +0100

    Record

--- src/JavaAdvanced/modulo2/ex6/Main.java
+++ src/JavaAdvanced/modulo2/ex6/Main.java
@@ -1,4 +1,8 @@
 package JavaAdvanced.modulo2.ex6;
 /*Definisci un oggetto Record che rappresenti un Punto con le coordinate x & y*/
 public class Main {
+    public static void main(String[] args) {
+        Punto punto = new Punto(3,6);
+        System.out.println(punto);
+    }
 }
---
+++ src/JavaAdvanced/modulo2/ex6/Punto.java
@@ -0,0 +1,5 @@
+package JavaAdvanced.modulo2.ex6;
+
+public record Punto(int x, int y) {
+
+}

843ab35b4494a13d9dd231b3b7170c246bebd59e
kappa-git <kevin.borgia@outlook.it>
2023-12-05 15:52:34 +0100

    HashMap

--- src/JavaAdvanced/modulo2/ex4/Main.java
+++ src/JavaAdvanced/modulo2/ex4/Main.java
@@ -1,4 +1,33 @@
 package JavaAdvanced.modulo2.ex4;
+
+import java.util.AbstractMap;
+import java.util.HashMap;
+import java.util.Map;
+
 /*Creare 3 hashmap con 3 differenti metodi di inizializzazione e stampare il contenuto.*/
 public class Main {
+    public static void main(String[] args) {
+        // Primo metodo di inizializzazione:
+        Map<String,String> mappa = new HashMap<String,String>();
+        mappa.put("Italia", "Roma");
+        mappa.put("United Stated of America", "Washington");
+        mappa.put("Thailandia", "Bangkok");
+        System.out.println(mappa);
+
+        // Secondo metodo di inizializzazione:
+        Map<String,String> maps = Map.of("PA","Palermo","TO","Torino","MI","Milano");
+        System.out.println(maps);
+
+        //Terzo metodo di inizializzazione:
+        Map<String,String> CAPmaps = new HashMap<>(Map.ofEntries(
+                new AbstractMap.SimpleEntry<String,String>("00010", "Gallicano nel lazio"),
+                new AbstractMap.SimpleEntry<String,String>("00036", "Palestrina"),
+                new AbstractMap.SimpleEntry<String,String>("00039", "Zagarolo")
+        ));
+        System.out.println(CAPmaps);
+
+
+
+
+    }
 }

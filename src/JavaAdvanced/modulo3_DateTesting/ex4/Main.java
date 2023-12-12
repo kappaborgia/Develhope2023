025d690ecb3ae3bb24b067a60fec9630ae3538f5
kappa-git <kevin.borgia@outlook.it>
2023-12-12 17:01:33 +0100

    date

--- src/JavaAdvanced/modulo3_DateTesting/ex4/Main.java
+++ src/JavaAdvanced/modulo3_DateTesting/ex4/Main.java
@@ -1,8 +1,23 @@
 package JavaAdvanced.modulo3_DateTesting.ex4;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Locale;
+
 /*Crea un oggetto data da questa stringa 2023-03-01T13:00:00Z
 aggiungi un anno
 sottrai un mese
 aggiungi 7 giorni
 Stampa il risultato localizzata per l'Italia*/
 public class Main {
+    public static void main(String[] args) {
+        OffsetDateTime myObj = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+
+        OffsetDateTime newObj = myObj.plusYears(1).minusMonths(1).plusDays(7);
+        String date = newObj.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM ));
+
+        System.out.println(date);
+    }
 }

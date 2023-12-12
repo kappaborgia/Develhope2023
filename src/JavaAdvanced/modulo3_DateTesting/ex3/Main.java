c819102b7dc18500b5b57aa68dcae716aa9e9f65
kappa-git <kevin.borgia@outlook.it>
2023-12-12 17:01:24 +0100

    date

--- src/JavaAdvanced/modulo3_DateTesting/ex3/Main.java
+++ src/JavaAdvanced/modulo3_DateTesting/ex3/Main.java
@@ -1,4 +1,10 @@
 package JavaAdvanced.modulo3_DateTesting.ex3;
+
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Date;
+
 /*Crea un oggetto data da questa stringa 2023-03-01T13:00:00Z
 ottieni l'anno
 ottieni il mese
@@ -6,4 +12,15 @@ ottieni il giorno
 ottieni il giorno della settimana
 Stampa i risultati sulla console*/
 public class Main {
+    public static void main(String[] args) {
+        OffsetDateTime date = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        System.out.println(date.format(DateTimeFormatter.ofPattern("yyyy")));
+        System.out.println(date.format(DateTimeFormatter.ofPattern("MMMM")));
+        System.out.println(date.format(DateTimeFormatter.ofPattern("dd")));
+        System.out.println(date.format(DateTimeFormatter.ofPattern("EEEE")));
+
+        String dateString = date.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.FULL, FormatStyle.SHORT));
+        System.out.println(dateString);
+
+    }
 }

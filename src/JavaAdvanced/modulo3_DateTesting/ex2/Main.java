62d943db36eb41dc5462129b91995680298dfcb4
kappa-git <kevin.borgia@outlook.it>
2023-12-12 17:01:06 +0100

    Date

--- src/JavaAdvanced/modulo3_DateTesting/ex2/Main.java
+++ src/JavaAdvanced/modulo3_DateTesting/ex2/Main.java
@@ -1,6 +1,16 @@
 package JavaAdvanced.modulo3_DateTesting.ex2;
+
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
 /*Crea un oggetto OffsetDateTime da questa stringa 2023-03-01T13:00:00Z
 Formatta la data ottenendo 01 marzo 2023
 Stampa sulla console*/
 public class Main {
+    public static void main(String[] args) {
+        OffsetDateTime Oggetto = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        String dateString = Oggetto.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG));
+        System.out.println(dateString);
+    }
 }

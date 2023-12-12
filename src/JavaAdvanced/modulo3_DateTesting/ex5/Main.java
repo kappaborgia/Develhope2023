76c372bace2a09b5866e72dc4712f8e7ccebd7be
kappa-git <kevin.borgia@outlook.it>
2023-12-12 17:01:49 +0100

    date

--- src/JavaAdvanced/modulo3_DateTesting/ex5/Main.java
+++ src/JavaAdvanced/modulo3_DateTesting/ex5/Main.java
@@ -1,4 +1,7 @@
 package JavaAdvanced.modulo3_DateTesting.ex5;
+
+import java.time.OffsetDateTime;
+
 /*Crea un oggetto data da questa stringa 2023-03-01T13:00:00Z
 Crea un secondo oggetto data da questa stringa 2024-03-01T13:00:00Z
 verificare che la prima data è precedente alla seconda
@@ -6,4 +9,14 @@ verificare che la seconda data è successiva alla prima
 verificare che le due date sono uguali ad ora
 Stampa il risultato*/
 public class Main {
+    public static void main(String[] args) {
+        OffsetDateTime myDate1 = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        OffsetDateTime myDate2 = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+
+        if (myDate1.equals(myDate2)){
+            System.out.println("Le due date sono uguali!");
+        }else {
+            System.out.println("Le due date sono differenti!");
+        }
+    }
 }

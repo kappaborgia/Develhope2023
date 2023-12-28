102473cfb7ad6549d75421c2ec4317b8cef8ec70
kappa-git <kevin.borgia@outlook.it>
2023-12-29 00:24:35 +0100

    Operatore Ternario

--- src/JavaAdvanced/modulo4_DesignPattern/ex2/Main.java
+++ src/JavaAdvanced/modulo4_DesignPattern/ex2/Main.java
@@ -1,4 +1,32 @@
 package JavaAdvanced.modulo4_DesignPattern.ex2;
+/*Copia il seguente programma Java e sostituisci il costrutto if-else con l'operatore ternario
 
+public class Module01 {
+    public static void main(String[] args) {
+        System.out.println(checkString("Questa è una stringa"));
+    }
+
+    private static String checkString(String value) {
+        String result;
+        if (value.length() >= 10) {
+            result = "Lunghezza maggiore o uguale di 10";
+        } else {
+            result = "Lunghezza minore di 10";
+        }
+        return result;
+    }
+}*/
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(checkString("Questa  è una stringa"));
+
+    }
+    private static String checkString(String value){
+        final String result = value.length() >= 10
+                ? "Lunghezza maggiore o uguale a 10"
+                : "Lunghezza minore di 10";
+        return result;
+    }
+
 }
+
